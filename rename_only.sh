5d777a45d8d002f393fea02dac0891acefc955fc
Matt Brown <mrbrown@iastate.edu>
2015-06-29 08:17:17 -0500

    fixed quote issue
    fixed issue created by using variables inside of single quotes. see: https://stackoverflow.com/questions/840536/how-to-use-environment-variable-inside-a-quoted-string-in-bash-script

--- rename_only.sh
+++ rename_only.sh
@@ -3,9 +3,9 @@
 CURNAME="myVM1"
 NEWNAME="myVM2"
 
-find -type f -name '*CURNAME*' | sed -e "p;s/CURNAME/NEWNAME/" | xargs -n2 mv
+find -type f -name "*$CURNAME*" | sed -e "p;s/$CURNAME/$NEWNAME/" | xargs -n2 mv
 
-sed -i 's/$CURNAME/$NEWNAME/g' $NEWNAME.vmdk
-sed -i 's/$CURNAME/$NEWNAME/g' $NEWNAME.vmx
-sed -i 's/$CURNAME/$NEWNAME/g' $NEWNAME.vmxf
-sed -i 's/$CURNAME/$NEWNAME/g' *.log
+sed -i "s/$CURNAME/$NEWNAME/g" $NEWNAME.vmdk
+sed -i "s/$CURNAME/$NEWNAME/g" $NEWNAME.vmx
+sed -i "s/$CURNAME/$NEWNAME/g" $NEWNAME.vmxf
+sed -i "s/$CURNAME/$NEWNAME/g" *.log
