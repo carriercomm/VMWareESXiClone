276b5dcd5cae9a68c8b6ab7447eec44f7f1c352d
Matt Brown <mrbrown@iastate.edu>
2015-06-29 08:31:44 -0500

    fixed quote issue
    fixed issue created by using variables inside of single quotes. see: https://stackoverflow.com/questions/840536/how-to-use-environment-variable-inside-a-quoted-string-in-bash-script

--- clone_and_rename.sh
+++ clone_and_rename.sh
@@ -6,9 +6,9 @@ NEWNAME="myVM2"
 cp -r $CURNAME $NEWNAME
 cd $NEWNAME
 
-find -type f -name '*CURNAME*' | sed -e "p;s/CURNAME/NEWNAME/" | xargs -n2 mv
+find -type f -name "*$CURNAME*" | sed -e "p;s/$CURNAME/$NEWNAME/" | xargs -n2 mv
 
-sed -i 's/$CURNAME/$NEWNAME/g' $NEWNAME.vmdk
-sed -i 's/$CURNAME/$NEWNAME/g' $NEWNAME.vmx
-sed -i 's/$CURNAME/$NEWNAME/g' $NEWNAME.vmxf
-sed -i 's/$CURNAME/$NEWNAME/g' *.log
+sed -i "s/$CURNAME/$NEWNAME/g" $NEWNAME.vmdk
+sed -i "s/$CURNAME/$NEWNAME/g" $NEWNAME.vmx
+sed -i "s/$CURNAME/$NEWNAME/g" $NEWNAME.vmxf
+sed -i "s/$CURNAME/$NEWNAME/g" *.log
